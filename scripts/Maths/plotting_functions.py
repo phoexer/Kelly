e4c2f2523fd366acbf4cdfddb078f23623ed7b87
phoexer <phoexer@gmail.com>
2017-08-19 22:49:30 +0200

    Did some maths
    I'm starting a degree in applied maths and statistics soon so I'm just brushing up on my stuff.

---
+++ scripts/Maths/plotting_functions.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 19 21:40:35 2017
+
+@author: michael
+
+Simple script that draws the graph of a function
+I was figuring this out as I went along, just to see if I can do it.
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+#Constants
+a = 1
+b = 2
+c = 3
+#plt.plot([2,4,6,8],[1,2,3,4],'ro')
+#plt.ylabel('Some Numbers')
+
+def sinFunc(x):
+    """f(x) = sin(x)"""
+    return math.sin(x) 
+
+def quadraticFunc(x):
+    """y = ax^2 + bx + c,"""
+    return a*x**2 + b*x + c
+
+def powerFunc(x):
+    """y = ax^c"""
+    return a*x**c
+
+def polinomialFunc(x):
+    """y = x^5 -8x^3 +10x + 6"""
+    return x**5 - 8*x**3 + 10*x + 6
+
+x = np.arange(-3., 3., .2) 
+
+sy = [sinFunc(y) for y in x]
+qy = [quadraticFunc(y) for y in x]
+py = [powerFunc(y) for y in x]
+ply = [polinomialFunc(y) for y in x]
+
+#T = np.matrix(t)
+
+plt.subplot(2,2,1)
+plt.plot(x, sy,'r-.')
+plt.grid(True)
+
+plt.subplot(2,2,2)
+plt.plot(x,qy,'b-.')
+plt.grid(True)
+
+plt.subplot(2,2,3)
+plt.plot(x,py,'g-.')
+plt.grid(True)
+
+plt.subplot(2,2,4)
+plt.plot(x,ply,'y-.')
+plt.grid(True)
+
+plt.show()
